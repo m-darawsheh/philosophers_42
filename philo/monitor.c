1a2e497219ea52f75933b5835ca102f452c6fd28
m-darawsheh <mohamaddarawsha2005@gmail.com>
2025-02-10 16:53:09 +0300

    fixed data race when num philo more than 50

--- philo/monitor.c
+++ philo/monitor.c
@@ -6,7 +6,7 @@
 /*   By: mdarawsh <mdarawsh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 14:06:58 by mdarawsh          #+#    #+#             */
-/*   Updated: 2025/02/07 16:06:25 by mdarawsh         ###   ########.fr       */
+/*   Updated: 2025/02/10 16:28:37 by mdarawsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 int	exit_routine(t_table *table, int l)
 {
+	pthread_mutex_lock(&table->write_mutex);
 	int	current_time;
-
 	current_time = get_time();
 	if ((current_time - table->philo[l].when_ate) > table->time_to_die)
 	{
+		pthread_mutex_lock(&table->spare);
 		table->is_dead = 0;
+		pthread_mutex_unlock(&table->spare);
 		printf("[%d] philo number %d is dead\n",
 			current_time - table->times_start,
 			table->philo[l].id);
@@ -33,6 +35,7 @@ int	exit_routine(t_table *table, int l)
 		pthread_mutex_unlock(&table->write_mutex);
 		return (0);
 	}
+	pthread_mutex_unlock(&table->write_mutex);
 	return (1);
 }
 
@@ -44,13 +47,12 @@ void	*r_routine(void *args)
 	table = (t_table *)args;
 	while (table->there_is_meal || table->is_dead)
 	{
+		// pthread_mutex_lock(&table->spare);
 		l = 0;
 		while (l < table->numbers)
 		{
-			pthread_mutex_lock(&table->write_mutex);
 			if (!exit_routine(table, l))
 				return (NULL);
-			pthread_mutex_unlock(&table->write_mutex);
 			l++;
 		}
 		usleep(100);
