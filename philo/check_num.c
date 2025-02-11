9fd35df701b8011d9e5edc6d4fa9188d999e0fff
m-darawsheh <mohamaddarawsha2005@gmail.com>
2025-02-11 17:43:23 +0300

    change  var name

--- philo/check_num.c
+++ philo/check_num.c
@@ -6,18 +6,18 @@
 /*   By: mdarawsh <mdarawsh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 00:20:47 by mdarawsh          #+#    #+#             */
-/*   Updated: 2025/02/03 19:21:28 by mdarawsh         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:24:38 by mdarawsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ft_usleep(long long start_time, long long duration_ms, t_table *table)
+void	ft_usleep(long long start_time, long long type_time, t_table *table)
 {
-	long long	end_time;
+	long long	the_end;
 
-	end_time = start_time + duration_ms;
-	while (get_time() < end_time)
+	the_end = start_time + type_time;
+	while (get_time() < the_end)
 	{
 		pthread_mutex_lock(&table->write_mutex);
 		if (!table->is_dead)
